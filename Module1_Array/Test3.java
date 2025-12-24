bff863ffe1a61405ff69f8e4e7fab4e84e31f8d0
YashrajBhati <yashrajbhati36@gmail.com>
2025-12-24 12:00:33 +0530

    Add Student class with name and age attributes

---
+++ Module1_Array/Test3.java
@@ -0,0 +1,13 @@
+class Student{
+  
+        String name;
+        int age;
+
+public static void main(String args[]){
+    Student b = new Student();
+    b.name="jai";
+    b.age= 20;
+    System.out.println("name:"+b.name);
+    System.out.println("age:"+b.age);
+}
+}
